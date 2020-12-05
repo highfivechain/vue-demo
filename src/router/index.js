50bbe43c7ee3c7e110cf3fb476feca4e14515e95
LAPTOP-Q78GBO4P\PeiChong <807168347@qq.com>
2020-12-05 11:42:05 +0800

    新建first.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from '@/components/view/First'
 
 Vue.use(Router)
 
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },{
+      path:'/first',
+      name:'First',
+      component:First
     }
   ]
 })
